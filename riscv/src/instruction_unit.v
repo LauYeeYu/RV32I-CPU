f61ee98b0194e5a20a15b763a6dcb623e7808eab
Lau Yee-Yu <liuyiyu2003@126.com>
2022-12-15 22:37:55 +0800

    feat: stall function for instruction unit

--- riscv/src/instruction_unit.v
+++ riscv/src/instruction_unit.v
@@ -6,19 +6,25 @@ module InstructionUnit(
   input  wire [31:0] instrAddr,     // instruction address (icache)
   input  wire        rsFull,        // reservation station full signal
   input  wire        robFull,       // reorder buffer full signal
+  input  wire        robReady,      // reorder buffer ready signal
+  input  wire [3:0]  robNext,       // reorder buffer next free slot
+  input  wire [31:0] robValue,      // reorder buffer value
   input  wire        lsbFull,       // load/store buffer full signal
   input  wire        jump,          // jump signal (predictor)
-  input  wire        enable,        // enable signal (Reorder Buffer)
   output wire        instrOutValid, // instruction output valid signal (PC)
-  output wire [31:0] instrAddrOut   // instruction address (PC)
+  output wire [31:0] instrAddrOut,  // instruction address (PC)
+  output wire [3:0]  robRequest     // reorder buffer request
 );
 
 reg [31:0] PC;
 reg [31:0] instrReg; // for instrction decode and issue
+reg        instrRegValid;
 reg        stall;
+reg [3:0]  stallDependency;
 
 assign instrOutValid = ~stall;
 assign instrAddrOut  = PC;
+assign robRequest    = stallDependency;
 
 // Utensils for fetching instruction
 wire lsbUsed = (instrIn[6:0] == 7'b0000011) || (instrIn[6:0] == 7'b0100011);
@@ -42,19 +48,27 @@ wire [31:0] shiftAmount  = {27'b0, instrReg[24:20]};
 
 always @(posedge clockIn) begin
   if (resetIn) begin
-    PC    <= 32'b0;
-    stall <= 1'b0;
+    PC              <= 32'b0;
+    stall           <= 1'b0;
+    stallDependency <= 4'b0000;
+    instrRegValid   <= 1'b0;
   end else begin
-    if (enable && stall) stall <= 1'b0;
-    if (~full && ~stall && instrInValid) begin
+    if (stall && robReady) begin
+      stall         <= 1'b0;
+      instrRegValid <= 1'b1;
+    end else if (~full && instrInValid) begin
       instrReg <= instrIn;
+      instrRegValid <= 1'b1;
       if (instrIn[6:0] == 7'b1100011) begin // branch
         PC <= jump ? PC + branchDiff : PC + 4;
       end else if (instrIn[6:0] == 7'b1100111) begin // JALR
-        stall <= 1'b1;
+        stall           <= 1'b1;
+        stallDependency <= instrRegValid ? robNext + 1 : robNext;
       end else begin // Other instructions
         PC <= PC + 4;
       end
+    end else begin
+      instrRegValid <= 1'b0;
     end
   end
 end
