c1df2d18ac23c2e569e59839d492b42e55644490
Lau Yee-Yu <liuyiyu2003@126.com>
2022-12-17 16:31:40 +0800

    refactor: optimize the implementation of predictor to make the time spent on each step more similar

--- riscv/src/predictor.v
+++ riscv/src/predictor.v
@@ -1,32 +1,29 @@
 module predictor #(
   parameter LOCAL_WIDTH = 12,
-  parameter LOCAL_SIZE = 2**LOCAL_WIDTH,
+  parameter LOCAL_SIZE = 2**LOCAL_WIDTH
 ) (
   input  wire        resetIn,       // resetIn
   input  wire        clockIn,       // clockIn
   input  wire        instrInValid,  // instruction valid signal (icache)
-  input  wire [31:0] instrIn,       // data valid signal (icache)
   input  wire [31:0] instrAddr,     // instruction address (icache)
   input  wire        updateValid,   // update valid signal (Reorder Buffer)
   input  wire [31:0] updateInstr,   // instruction (Reorder Buffer)
   input  wire        taken,         // taken signal (Reorder Buffer)
-  output wire        jump,          // jump signal
+  output wire        jump           // jump signal
 );
 
 reg [LOCAL_WIDTH-1:0] localHistory[1:0];
+reg  [LOCAL_WIDTH-1:0] instrPos;
 
-wire instrPos = instrAddr[LOCAL_WIDTH+1:2];
-wire updatePos = updateInstr[LOCAL_WIDTH+1:2];
+wire [LOCAL_WIDTH-1:0] updatePos = updateInstr[LOCAL_WIDTH+1:2];
 
-reg jumpReg;
-
-assign jump = jumpReg;
+assign jump = localHistory[instrPos] > 2'b01 ? 1'b1 : 1'b0;
 
 always @(posedge clockIn) begin
   if (resetIn) begin
     localHistory <= {LOCAL_SIZE{2'b01}};
   end else begin
-    jumpReg = localHistory[instrPos] > 2'b01 ? 1'b1 : 1'b0;
+    instrPos <= instrAddr[LOCAL_WIDTH+1:2];
     if (updateValid) begin
       localHistory[updatePos] <= taken ? localHistory[updatePos] + 2'b01 : localHistory[updatePos] - 2'b01;
     end
